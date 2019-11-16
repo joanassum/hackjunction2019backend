647ff0f5af5d238965b819e566537f00226275e4
Joan Tso <joantsomw@yahoo.com.hk>
2019-11-16 22:48:41 +0000

    integrate gramma and rules check

--- hello.py
+++ hello.py
@@ -1,17 +1,11 @@
 from flask import Flask
 from flask import request
 import alerts1
+import page_checker
 
 app = Flask(__name__)
 
 @app.route('/check-phishing')
 def hello_world():
     url = request.args.get('url')
-    data = {}
-    data['isPhishing'] = is_phishing(url)
-    return data
-
-def is_phishing(url):
-    print(url)
-    print(alerts1.computeAlerts(url))
-    return alerts1.computeAlerts(url) != []
+    return page_checker.get_issues(url)
